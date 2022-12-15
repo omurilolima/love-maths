d89b59b4862e4fb3759da478a45f9723c8ed3095
Murilo Lima <msl@cesar.school>
2022-12-15 14:33:00 +0000

    Add code to calculate twu random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,14 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 })
-
+/**
+ * The main game "loop", called when the script is first loaded 
+ * and after the user´s answer has been processed
+ */
 function runGame() {
-
+    // creates 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = math.floor(math.random() * 25) + 1;
 }
 
 function checkAnswer() {
